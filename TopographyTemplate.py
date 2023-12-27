58b2611bbb0c63947fab1e85a104f345b913ac5a
Christopher Lycan <96343159+777388@users.noreply.github.com>
2023-12-27 14:49:39 -0800

    Update TopographyTemplate.py

--- TopographyTemplate.py
+++ TopographyTemplate.py
@@ -1,16 +1,17 @@
 import sys
-instance = []
+class instantiate:
+    instance = []
 if (sys.argv[2] == "number"):
     for i in range(int(sys.argv[1])):
-        instance.append(i)
+        instantiate.instance.append(i)
 if (sys.argv[2] == "string"):
     for I in sys.argv[1]:
-        instance.append(I)
+        instantiate.instance.append(I)
 if (sys.argv[2] == "file"):
     with open(sys.argv[1], "r") as f:
         for line in f:
             for character in line:
-                instance.append(character)
+                instantiate.instance.append(character)
 topography = lambda node: print(node, end="")
 
-(list(map(topography, instance)))
+(list(map(topography, instantiate.instance)))
